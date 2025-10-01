096a3e6107ba14e74e0a9134b158ff9af6fd399b
VictorS <victorsantos12dima@gmail.com>
2025-10-01 12:21:56 -0300

    feat: add config redux, redux saga, reducer, types

---
+++ src/store/modules/auth/sagas.js
@@ -0,0 +1,8 @@
+import { call, put, all, takeLatest } from 'redux-saga/effects';
+import * as types from '../types';
+
+function* loginRequest(payload) {
+  console.log('SAGA', payload);
+}
+
+export default all([takeLatest(types.LOGIN_REQUEST, loginRequest)]);
