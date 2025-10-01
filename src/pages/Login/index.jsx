88b777fbed7a1828d6b547ab100f0cda83f2bdef
VictorS <victorsantos12dima@gmail.com>
2025-10-01 17:03:38 -0300

    feat: login flow /2

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,10 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import isEmail from 'validator/lib/isEmail';
+import { toast } from 'react-toastify';
+import { get } from 'lodash';
+import { useDispatch, useSelector } from 'react-redux';
 import { Container } from '../../styles/GlobalStyles';
+import { Form } from './styled';
+import { loginRequest } from '../../store/modules/auth/actions';
+import Loading from '../../components/Loading';
 
-function Login() {
+function Login(props) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+
+  const prevPath = get(props, 'location.state.prevPath', '/');
+
+  const isLoading = useSelector((state) => state.auth.loading);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let formErrors = false;
+
+    if (!isEmail(email)) {
+      formErrors = true;
+      toast.error('Email inválido');
+    }
+
+    if (password.length < 6 || password.length > 50) {
+      formErrors = true;
+      toast.error('Senha inválida');
+    }
+
+    if (formErrors) return;
+
+    dispatch(loginRequest({ email, password, prevPath }));
+  };
+
   return (
     <Container>
+      <Loading isLoading={isLoading} />
       <h1>Login</h1>
+
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Seu email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Sua  senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Acessar</button>
+      </Form>
     </Container>
   );
 }
