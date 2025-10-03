9aa0e63ef8edcaabb4b4b3ace45b77671263132c
VictorS <victorsantos12dima@gmail.com>
2025-10-03 14:53:22 -0300

    feat: add logout

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
+import {
+  FaCircle,
+  FaHome,
+  FaPowerOff,
+  FaSignInAlt,
+  FaUserAlt,
+} from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import { Nav } from './styled';
+import * as actions from '../../store/modules/auth/actions';
 
 function Header() {
+  const dispatch = useDispatch();
+  const { isLoggedIn } = useSelector((state) => state.auth);
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(actions.loginFailure());
+  };
+
   return (
     <Nav>
       <Link to="/">
@@ -12,9 +28,17 @@ function Header() {
       <Link to="/register">
         <FaUserAlt size={24} />
       </Link>
-      <Link to="/login">
-        <FaSignInAlt size={24} />
-      </Link>
+      {isLoggedIn ? (
+        <Link onClick={handleLogout} to="/logout">
+          <FaPowerOff size={24} />
+        </Link>
+      ) : (
+        <Link to="/login">
+          <FaSignInAlt size={24} />
+        </Link>
+      )}
+
+      {isLoggedIn && <FaCircle size={24} color="#66ff33" />}
     </Nav>
   );
 }
