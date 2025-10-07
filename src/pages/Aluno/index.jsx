4973c7e611201a189ed34b18233231603a29be0c
VictorS <victorsantos12dima@gmail.com>
2025-10-07 14:30:50 -0300

    feat: add form, validation to create/edit Aluno

--- src/pages/Aluno/index.jsx
+++ src/pages/Aluno/index.jsx
@@ -1,12 +1,202 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { get } from 'lodash';
+import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
+import isEmail from 'validator/lib/isEmail';
+import { isFloat, isInt } from 'validator';
+import { useDispatch } from 'react-redux';
 import { Container } from '../../styles/GlobalStyles';
+import { Form } from './styled';
+import Loading from '../../components/Loading';
+import api from '../../services/axios';
+import history from '../../services/history';
+import * as actions from '../../store/modules/auth/actions';
 
-function Aluno() {
+function Aluno({ match }) {
+  const id = get(match, 'params.id', '');
+  const [nome, setNome] = useState('');
+  const [sobrenome, setSobrenome] = useState('');
+  const [email, setEmail] = useState('');
+  const [idade, setIdade] = useState('');
+  const [altura, setAltura] = useState('');
+  const [peso, setPeso] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!id) return;
+
+    async function getData() {
+      try {
+        setIsLoading(true);
+        const { data } = await api.get(`/alunos/${id}`);
+        const Foto = get(data, 'Fotos[0].url', '');
+        setNome(data.nome);
+        setSobrenome(data.sobrenome);
+        setEmail(data.email);
+        setIdade(data.idade);
+        setAltura(data.altura);
+        setPeso(data.peso);
+        setIsLoading(false);
+      } catch (err) {
+        setIsLoading(false);
+        const status = get(err, 'response.status', 0);
+        const errors = get(err, 'response.data.errors', []);
+
+        if (status === 400) {
+          errors.map((error) => toast.error(error));
+          history.push('/');
+        }
+
+        if (status === 401) {
+          toast.error('Você precisa fazer login novamente.');
+          history.push('/login');
+        }
+        toast.error('Erro ao buscar aluno');
+        setIsLoading(false);
+      }
+    }
+
+    getData();
+  }, [id]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    let formErrors = false;
+
+    if (nome.length < 3 || nome.length > 255) {
+      toast.error('Nome deve ter entre 3 e 255 caracteres');
+      formErrors = true;
+    }
+
+    if (sobrenome.length < 3 || sobrenome.length > 255) {
+      toast.error('Sobrenome deve ter entre 3 e 255 caracteres');
+      formErrors = true;
+    }
+
+    if (!isEmail(email)) {
+      toast.error('Email inválido');
+      formErrors = true;
+    }
+
+    if (!isInt(String(idade))) {
+      toast.error('Idade inválida');
+      formErrors = true;
+    }
+
+    if (!isFloat(String(peso))) {
+      toast.error('Peso inválido');
+      formErrors = true;
+    }
+
+    if (!isFloat(String(altura))) {
+      toast.error('Altura inválida');
+      formErrors = true;
+    }
+
+    if (formErrors) return;
+
+    try {
+      setIsLoading(true);
+      if (id) {
+        await api.put(`/alunos/${id}`, {
+          nome,
+          sobrenome,
+          email,
+          idade,
+          peso,
+          altura,
+        });
+        toast.success('Aluno(a) editado(a) com sucesso');
+        history.push('/');
+      } else {
+        const { data } = await api.post('/alunos', {
+          nome,
+          sobrenome,
+          email,
+          idade,
+          peso,
+          altura,
+        });
+        toast.success('Aluno(a) criado com sucesso');
+        history.push(`/aluno/${data.id}/edit`);
+      }
+      setIsLoading(false);
+    } catch (err) {
+      setIsLoading(false);
+      const status = get(err, 'response.status', 0);
+      const errors = get(err, 'response.data.errors', []);
+      if (errors.length > 0) {
+        errors.map((error) => toast.error(error));
+      } else {
+        toast.error('Erro desconhecido');
+      }
+      if (status === 401) {
+        dispatch(actions.loginFailure());
+        toast.error('Você precisa fazer login novamente.');
+        history.push('/login');
+      }
+      setIsLoading(false);
+    }
+  };
+
   return (
     <Container>
-      <h1>Aluno</h1>
+      <Loading isLoading={isLoading} />
+      <h1>{id ? 'Editar aluno' : 'Novo aluno'}</h1>
+
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+          placeholder="Seu nome"
+        />
+
+        <input
+          type="text"
+          value={sobrenome}
+          onChange={(e) => setSobrenome(e.target.value)}
+          placeholder="Seu sobrenome"
+        />
+
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Seu email"
+        />
+
+        <input
+          type="number"
+          value={idade}
+          onChange={(e) => setIdade(e.target.value)}
+          placeholder="Seu idade"
+        />
+
+        <input
+          type="number"
+          value={peso}
+          onChange={(e) => setPeso(e.target.value)}
+          placeholder="Seu peso"
+        />
+
+        <input
+          type="number"
+          value={altura}
+          onChange={(e) => setAltura(e.target.value)}
+          placeholder="Sua altura"
+        />
+
+        <button type="submit">Enviar</button>
+      </Form>
     </Container>
   );
 }
+
+Aluno.propTypes = {
+  match: PropTypes.shape({}).isRequired,
+};
 
 export default Aluno;
---
+++ src/pages/Aluno/styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+
+  input {
+    margin-bottom: 20px;
+    height: 40px;
+    border: 1px solid #ddd;
+    padding: 0 10px;
+    border-radius: 4px;
+  }
+`;
